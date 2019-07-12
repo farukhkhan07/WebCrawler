c7b4721dfbf37a46e9a3891fb5e88061981a99b9
farukhkhan07 <farukhcs15@gmail.com>
2019-07-12 16:25:03 +0200

    .netcore webCrawler

---
+++ dotNetWebCrawler/dotNetWebCrawler/Cars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerDemo
+{
+    class Cars
+    {
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Link { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ dotNetWebCrawler/dotNetWebCrawler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace CrawlerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startCrawlerasync();
+            Console.ReadLine();
+        }
+
+        private static async Task startCrawlerasync()
+        {
+            var url = "https://www.automobile.tn/fr/neuf/bmw";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+
+            var cars = new List<Cars>();
+            var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("versions-item")).ToList();
+
+            foreach(var div in divs)
+            {
+                var car = new Cars
+                {
+                    Model = div.Descendants("h2").FirstOrDefault().InnerText,
+                    Price = div.Descendants("div").FirstOrDefault().InnerText,
+                    Link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value,
+                    ImageUrl = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value
+
+                };
+                cars.Add(car);
+            }
+        }
+    }
+}
